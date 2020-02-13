307be57fc4c38f0964a787f379e0ef44bfbd761f
dararan <pandaman.fight@gmail.com>
2020-02-13 11:47:38 +0900

    一覧機能作成

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php
+// 一覧表示機能
+//   ①データの取得（タスク）
+//     DBから読み込み（接続）
+    //    DBの接続
+    //    SQLの実行（データベースを操作する言語）
+    //      READ(select)
+    //    結果を変数に代入する
+
+
+//   ②１のデータを画面に表示する
+
+// ファイルの読み込み
+require_once('Models/Task.php');
+// getAllを使いたい
+// タスククラスのメソッド
+// メソッドを使う場合はクラスをインスタンス化しないといけない
+ 
+// タスククラスをインスタンス化して。$Taskにインスタンスを代入
+// タスクの中のゲットオールを使いたかった
+// echo'<pre>';
+$todo = new Task();
+// var_dump($todo);die;
+
+$tasks = $todo->getAll();
+// var_dump($tasks);die;
 
 ?>
 <!DOCTYPE html>
@@ -44,13 +69,14 @@
         </div>
 
         <div class="row p-3">
+        <?php foreach ($tasks as $task):?>
             <div class="col-sm-6 col-md-4 col-lg-3 py-3 py-3">
                 <div class="card">
                     <img src="https://picsum.photos/200" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">ここにタイトル</h5>
+                        <h5 class="card-title"><?= $task['title'];?>/h5>
                         <p class="card-text">
-                            ここに詳細
+                            <?=$task['contents'];?>
                         </p>
                         <div class="text-right d-flex justify-content-end">
                             <a href="edit.php" class="btn text-success">EDIT</a>
@@ -62,6 +88,7 @@
                     </div>
                 </div>
             </div>
+            <?php endforeach; ?>
         </div>
     </div>
 
